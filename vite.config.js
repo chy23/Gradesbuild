08ef487888e25a2c9d5c75558d752aaa895818d1
Chiahua <chiahua@Chiahua-MacBook-Pro-M1-2.local>
2026-07-24 22:56:38 +0800

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Gradesbuild/', // 設定 GitHub Pages 的基礎路徑
+  base: './', // 更改為相對路徑以防大小寫問題
 })
